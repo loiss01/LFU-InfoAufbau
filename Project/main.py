477f7fd9f4b254079d664acfd72a021bc3d17a44
alois <alois@steckei.com>
2023-01-04 20:09:39 +0100

    init gameproject

---
+++ Project/main.py
@@ -0,0 +1,14 @@
+#Snake Game in 10x10 grid
+
+GRIDSIZE = 10
+
+snakeSize = 2
+
+#Game Tick every 0.5 Seconds
+def move():
+    #check if player is pressing a key
+    pass
+
+
+def tick():
+    move()
