fdfb018146541efcf24c78203951c7798e5ff20a
Alois Stöckl <alois@steckei.com>
2023-01-06 01:14:06 +0100

    drawing done

--- Project/tiger.py
+++ Project/tiger.py
@@ -1,8 +1,9 @@
 from gpanel import *
+import time
 
 GRIDSIZE = 10
 
-data = [[None, "Snake", "Snake", "Snake", None, None, None, None, None, None],
+data = [[None, None, "Snake", "Snake", "Snake", "Snake", None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
@@ -11,9 +12,8 @@ data = [[None, "Snake", "Snake", "Snake", None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
         [None, None, None, None, None, None, None, None, None, None],
-        [None, None, None, "Apple", None, None, None, None, None, None]]
-
-makeGPanel(-1, GRIDSIZE + 1, -1, GRIDSIZE + 1)
+        [None, None, None, None, None, None, None, "Apple", None, None]]
+direction = ""
 
 
 def drawBoard():
@@ -25,10 +25,35 @@ def drawBoard():
         for u in range(len(data[i])):
             if data[i][u] == None: continue
             if data[i][u] == "Apple":
-                fill(i + 0.1, u + 0.1, "white", "red")
+                fill(koordTransformation_X(u) - 0.1, koordTransformation_Y(i) - 0.1, "white", "red")
                 continue
-            fill(i + 0.1, u + 0.1, "white", "blue")
+            fill(koordTransformation_X(u) - 0.1, koordTransformation_Y(i) - 0.1, "white", "blue")
 
+
+def onKeyPressed(key_code):
+    if (key_code == 38):
+        direction = "up"
+    if (key_code == 39):
+        direction = "rechts"
+    if (key_code == 40):
+        direction = "down"
+    if (key_code == 37):
+        diection = "left"
+    print("Keychanged ", key_code)
+
+
+def koordTransformation_X(x):
+    return x
+
+
+def koordTransformation_Y(y):
+    return GRIDSIZE - y
+
+
+makeGPanel(-1, GRIDSIZE + 1, -1, GRIDSIZE + 1, keyPressed=onKeyPressed)
 
 drawBoard()
-fill(1, 1, "white", "red")
+
+# print(koordTransformation(1,0))
+# print(koordTransformation(2,0))
+# print(koordTransformation(3,0))
